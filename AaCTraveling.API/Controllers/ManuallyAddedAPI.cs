6e59132e71f7313ea44156d6d32e091cde5ec35e
Aaron Yihan Chen <cyh_cosin@126.com>
2021-11-08 19:27:40 +1100

    first controller api

---
+++ AaCTraveling.API/Controllers/ManuallyAddedAPI.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AaCTraveling.API.Controllers {
+
+    [Route("api/manuallyaddedapi")]
+    [ApiController]
+    public class ManuallyAddedAPI {
+        [HttpGet]
+        public IEnumerable<string> Get() {
+            return new[] { "THIS IS FROM", "MANUALLY ADDED API" };
+        }
+    }
+}
--- AaCTraveling.API/Startup.cs
+++ AaCTraveling.API/Startup.cs
@@ -13,6 +13,7 @@ namespace AaCTraveling.API {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,9 +25,13 @@ namespace AaCTraveling.API {
             app.UseRouting();
 
             app.UseEndpoints(endpoints => {
-                endpoints.MapGet("/", async context => {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/", async context => {
+                //    await context.Response.WriteAsync("Hello FROM TEST!");
+                //});
+                //endpoints.MapGet("/test", async context => {
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+                endpoints.MapControllers();
             });
         }
     }
